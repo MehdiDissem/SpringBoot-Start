5d6688ea82648730605d641059be96141d590868
Mehdidissem <mehdissem@gmail.com>
2023-04-14 01:02:14 +0200

    added a bean in the model

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 import com.example.demo.student.Student;
+import com.example.demo.student.StudentService;
 
 @SpringBootApplication
 
@@ -13,5 +16,10 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public StudentService studentservice(){
+		return new StudentService();
+	}
+	
 
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -4,24 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 // import com.example.demo.student.Student;
 
-import java.time.LocalDate;
-import java.time.Month;
+// import java.time.LocalDate;
+// import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
+    
+    private final StudentService studentService ;
+    public StudentController(StudentService studentService){
+        this.studentService= studentService;
+    }
+
 //get mapping for student controller
     @GetMapping()
 	public List<Student> getStudents() {
-		return List.of(
-			new Student(
-				1L,
-				"Mehdi",
-				"Mehdi@gmail.com",
-				LocalDate.of(2000, Month.FEBRUARY, 5),
-				21
-			)
-		);
+		return studentService.getStudents();
 	}
 }
